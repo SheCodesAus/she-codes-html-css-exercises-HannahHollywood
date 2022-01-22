a60ef71419ce8a180ba2c68d3170a7d50c0f8855
HannahHollywood <70626629+HannahHollywood@users.noreply.github.com>
2022-01-22 10:11:38 +0800

    Updates to Intro to JS

--- intro-to-js/script.js
+++ intro-to-js/script.js
@@ -1,6 +1,24 @@
 window.onload = function() {
     // start
 
+    // identify (create) the div
+    let starterDiv = document.getElementById('starter-div');
+    // made the div
+    let firstHeading = document.createElement('h2');
+    // linking & adding stylings and content to the div
+    firstHeading.innerText = 'This is an H2 created with JS';
+    firstHeading.classList.add('js-header');
+
+    //  appending the content to the div
+    starterDiv.appendChild(firstHeading);
+
+    // identify another div and add stylings
+    let secondHeading = document.createElement('h2');
+    secondHeading.innerText = 'This is another H2 element with JS. Wow!';
+    secondHeading.classList.add('js-header');
+    secondHeading.id = 'second-h2';
+
+    starterDiv.appendChild(secondHeading);
     // end
 }
 
@@ -8,13 +26,14 @@ window.onload = function() {
 function showName(){
     console.log('showName');
     // start
-
+    let name = document.getElementById('name').value;
+    document.getElementById('greeting-text').innerText = 'Hi ' + name;
     // end
 }
 
 function changeColour(colour) {
     // start
-
+    document.body.style.backgroundColor = colour;
     // end
 }
 
@@ -24,6 +43,9 @@ function resetColour() {
 
 function toggleLightbulb() {
     // start
-
+    let lightbulb = document.getElementById('light');
+    lightbulb.classList.toggle('on');
+    // log how many clicks to the turn on the lightbulb
+    console.log(lightbulb);
     // end
 }
